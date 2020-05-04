08d3cf32154d688a706d9ffc9a5a0b8af941a10f
Samuel Allemang <samuel.allemang@gmail.com>
2020-05-04 14:27:29 +0000

    Initial commit exercise 2

---
+++ src/MovieCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+   	return (
+      <div className='movie-card'>
+	      <h2>Movie Name</h2>
+      </div>
+    )
+  }
+}
+
+export default MovieCard
