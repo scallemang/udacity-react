06eda9c3fdc09fb82ba21d1cee2c26573872d6d7
Sam Allemang <samuel.allemang@gmail.com>
2020-05-13 15:55:36 +0000

    Completing exercise 1

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+   input: '', 
+  }
+
+  updateInput = (input) => {
+    this.setState(() => ({
+      input,
+    }))
+  }
+  
+  render() {
+    const { input } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="container">
+          <input 
+    		type="text" 
+    		placeholder="Say Something"
+    		value={ input }
+            onChange={(event) => this.updateInput(event.target.value)}
+		  />
+          <p className="echo">Echo:</p>
+          <p>{ input }</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
